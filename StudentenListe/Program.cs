40bcb3102542809158070d60de437d9552bf3051
Dexter Brings <dexter.brings@gmail.com>
2024-10-25 22:39:26 +0200

    Projektdateien hinzufügen.

---
+++ StudentenListe/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StudentenListe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aufgabe: Erstelle ein Programm mit einer Liste von Studenten
+            // Verwende für die Studenten eine Klasse
+            // Jeder Student hat einen NC
+            // Berechne dann den durchschnittlichen NC aller Studenten mit einer Schleife
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student(1, "Jannick", 2.3f));
+            students.Add(new Student(2, "Peter", 1.8f));
+            students.Add(new Student(3, "Maria", 2.1f));
+
+            float totalGradePoints = 0;
+
+            foreach (Student student in students) 
+            {
+                totalGradePoints += student.GradePointAverage;
+            }
+
+            float nc = totalGradePoints / students.Count;
+
+            Console.WriteLine(nc);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ StudentenListe/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentenListe
+{
+    internal class Student
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public float GradePointAverage { get; private set; }
+
+
+        public Student(int id, string name, float gradePointAverage)
+        {
+            Id = id;
+            Name = name;
+            GradePointAverage = gradePointAverage;
+        }
+    }
+}
